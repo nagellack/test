8ba93b6acde303012775280288c304c4fb7050f1
nagellack <lukas.nagel@kitcheck.com>
2018-10-05 14:52:05 -0400

    COMMIT 2

--- lets_practice.py
+++ lets_practice.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 if __name__ == "__main__":
+    a = np.random.rand(4)
     q = np.random.rand(4)
     b = np.random.rand(4)
     c = np.random.rand(4)
-    print('Hello', q + b + c)
+    print('Hello', a + q + b + c)
