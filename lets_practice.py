8843d160a36a6e5deb906705c517419163edc620
nagellack <lukas.nagel@kitcheck.com>
2018-10-05 14:37:35 -0400

    commit after the second branch

--- lets_practice.py
+++ lets_practice.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 if __name__ == "__main__":
-    a = np.random.rand(3)
+    q = np.random.rand(3)
     b = np.random.rand(3)
     c = np.random.rand(3)
-    print('Hello', a + b + c)
+    print('Hello', q + b + c)
