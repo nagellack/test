3a057cbc76ffb1cded1707132e9db58c4c90cb79
nagellack <lukas.nagel@kitcheck.com>
2019-03-26 11:47:06 -0400

    commi2

--- lets_practice.py
+++ lets_practice.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 if __name__ == "__main__":
-    a = np.random.rand(6)
+    a = np.random.rand(5)
     q = np.random.rand(4)
-    b = np.random.rand(5)
-    c = np.random.rand(44)
+    b = np.random.rand(3)
+    c = np.random.rand(2)
     d = np.randon.rand(1)
-    a = np.random.rand(6)
-    q = np.random.rand(4)
-    b = np.random.rand(5)
-    c = np.random.rand(44)
-    d = np.randon.rand(1)
+    a = np.random.rand(0)
+    q = np.random.rand(1)
+    b = np.random.rand(2)
+    c = np.random.rand(3)
+    d = np.randon.rand(4)
     print('Hello', a + q + b + c)
