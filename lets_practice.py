59a8b72669a0bea3c633f26aecfab437e1acd50c
nagellack <lukas.nagel@kitcheck.com>
2019-03-26 11:37:36 -0400

    commi2

--- lets_practice.py
+++ lets_practice.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = np.random.rand(6)
     q = np.random.rand(4)
     b = np.random.rand(5)
-    c = np.random.rand(5)
+    c = np.random.rand(44)
     d = np.randon.rand(1)
     print('Hello', a + q + b + c)
