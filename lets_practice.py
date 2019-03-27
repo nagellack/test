8af6a3967d81a27e01df50e1dbb0fb65f5b71e1e
nagellack <lukas.nagel@kitcheck.com>
2019-03-27 16:26:04 -0400

    and this

--- lets_practice.py
+++ lets_practice.py
@@ -2,9 +2,4 @@ import numpy as np
 
 if __name__ == "__main__":
     a = np.random.rand(100)
-    q = np.random.rand(100)
-    b = np.random.rand(101)
-    c = np.random.rand(101)
-    d = np.randon.rand(101)
-    a = np.random.rand(100)
     print('Hello', a + q + b + c)
